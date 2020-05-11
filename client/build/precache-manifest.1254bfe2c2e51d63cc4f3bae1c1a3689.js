58a6ac175396cf8462fd1ef6e82eaa1ee43eceec
attaryz <markosberg123@gmail.com>
2020-05-11 23:53:52 +0200

    tutorial part2

---
+++ client/build/precache-manifest.1254bfe2c2e51d63cc4f3bae1c1a3689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b6fbb0e248197cf432f1857538302c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311ab125236490181cb7",
+    "url": "/static/css/main.009d6504.chunk.css"
+  },
+  {
+    "revision": "b535f31d46b3ffb619f0",
+    "url": "/static/js/2.151ffa8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.151ffa8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311ab125236490181cb7",
+    "url": "/static/js/main.1fe22400.chunk.js"
+  },
+  {
+    "revision": "cd50a7d19c7673d39246",
+    "url": "/static/js/runtime-main.4cada699.js"
+  }
+]);
